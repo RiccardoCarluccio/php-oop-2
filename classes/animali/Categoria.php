4867a9cae9b268e8c76ecdecb22fd2ee4098d5bd
RiccardoCarluccio <riccardo.carluccio.93@gmail.com>
2023-09-21 15:42:23 +0200

    do categoria exception

--- classes/animali/Categoria.php
+++ classes/animali/Categoria.php
@@ -1,8 +1,34 @@
 <?php
+  require_once __DIR__ . '/../exceptions/ValidationException.php';
+
   class Categoria {
     protected $razza;
     protected $taglia;
     protected $peso;
     protected $colore;
+
+    /**
+     * Get the value of colore
+     */ 
+    public function getColore()
+    {
+        return $this->colore;
+    }
+
+    /**
+     * Set the value of colore
+     *
+     * @return  self
+     */ 
+    public function setColore($colore)
+    {
+      if ($colore === "verde") {
+        throw new ValidationException('Colore non valido');
+      }
+
+        $this->colore = $colore;
+
+        return $this;
+    }
   }
 ?>
